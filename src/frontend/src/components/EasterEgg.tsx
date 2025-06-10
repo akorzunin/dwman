94a0cdb6dfd0df1c109f2e2b971d4ecfb5866f60
akorzunin <akorzunin123@gmail.com>
2025-06-10 23:39:03 +0300

    feat: new easter egg feature

--- src/frontend/src/components/EasterEgg.tsx
+++ src/frontend/src/components/EasterEgg.tsx
@@ -3,29 +3,33 @@ import { get_text_emoji } from '../utils/utils';
 import { useAtom } from 'jotai';
 import { easterEggCountAtom, easterEggKaomojiAtom } from '../store/store';
 import { cn } from '../lib/utils';
+import SplashCursor from './bg/splash_cursor/SplashCursor';
 
 export const EasterEgg = () => {
   const [easterEggCount, setEasterEggCount] = useAtom(easterEggCountAtom);
   const [rotate, setRotate] = useState(false);
   const [textEmoji, settextEmoji] = useAtom(easterEggKaomojiAtom);
   return (
-    <span
-      className={cn(
-        'select-none text-sm text-muted-foreground hover:cursor-help',
-        rotate && 'animate-spin'
-      )}
-      onClick={() => {
-        setEasterEggCount(easterEggCount + 1);
-        if (easterEggCount > 10) {
-          setRotate(true);
-          setEasterEggCount(0);
-        } else {
-          setRotate(false);
-        }
-        settextEmoji(get_text_emoji());
-      }}
-    >
-      {textEmoji}
-    </span>
+    <>
+      <span
+        className={cn(
+          'select-none text-sm text-muted-foreground hover:cursor-help',
+          rotate && 'animate-spin'
+        )}
+        onClick={() => {
+          setEasterEggCount(easterEggCount + 1);
+          if (easterEggCount > 10) {
+            setRotate(true);
+            setEasterEggCount(0);
+          } else {
+            setRotate(false);
+          }
+          settextEmoji(get_text_emoji());
+        }}
+      >
+        {textEmoji}
+      </span>
+      {rotate && <SplashCursor />}
+    </>
   );
 };
